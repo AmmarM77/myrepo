8f3e122a261f444016637d3df4fdb10101e83bc8
Ammar Mukadam <helloammarm@gmail.com>
2023-07-10 10:49:54 -0700

    Made changes

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
-print("hello world")
-
-print("whatsup")
 
 #lets make a quick edit here
+
+with open('hello.txt') as text_file:
+    text_data = text_file.readline()
+
+class Employee():
+  new_id = 1
+
+  def __init__(self):
+    self.id = Employee.new_id
+    Employee.new_id += 1
+  def say_id(self):
+    print("My id is " + str(self.id))
+
+e1 = Employee()
+e2 = Employee()
+e1.say_id()
+e2.say_id()
+
+    
+
+
+
